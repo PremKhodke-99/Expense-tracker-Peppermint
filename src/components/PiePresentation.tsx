4d302ae01e14be0ff0d75cd3e10c76baf709e44e
Prem <premkhodke.99@gmail.com>
2024-09-15 13:55:26 +0530

    custom type

--- src/components/PiePresentation.tsx
+++ src/components/PiePresentation.tsx
@@ -3,17 +3,26 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { PieChart, Pie, Cell } from "recharts";
 
+interface CustomType{
+  cx: number,
+  cy: number,
+  midAngle: number,
+  innerRadius: number,
+  outerRadius: number,
+  percent: number
+}
+
 const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
 
 const RADIAN = Math.PI / 180;
-const renderCustomizedLabel = ({
+const renderCustomizedLabel: React.FC<CustomType> = ({
   cx,
   cy,
   midAngle,
   innerRadius,
   outerRadius,
   percent,
-}: any) => {
+}) => {
   const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
   const x = cx + radius * Math.cos(-midAngle * RADIAN);
   const y = cy + radius * Math.sin(-midAngle * RADIAN);
